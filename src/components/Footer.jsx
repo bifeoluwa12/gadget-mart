1bbd533f534f54a06b5d90ff68354e556c41ea78
bifeoluwa12 <igwt3030@gmail.com>
2025-10-20 08:40:16 +0100

    Update footer content and attribution

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer id="contact" className="bg-blue-600 text-white text-center py-6 mt-12">
-      <p>© {new Date().getFullYear()} ShopEase. All rights reserved.</p>
-      <p>Made with ❤️ using React + Tailwind</p>
+      <p>© {new Date().getFullYear()} Gadgetsmart. All rights reserved.</p>
+      <p>Made with ❤️ by Babawale Ifeoluwa David using React + Tailwind</p>
     </footer>
   );
 }
